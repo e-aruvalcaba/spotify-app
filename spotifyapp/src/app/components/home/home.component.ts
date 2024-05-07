4692fa813638616cc21924059d0483896391aa83
e-aruvalcaba <adrian.ruvalcaba.sistemas@hotmail.com>
2024-05-07 03:40:31 -0600

    estructura inicial completa

--- spotifyapp/src/app/components/home/home.component.ts
+++ spotifyapp/src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../arquitecture/sidebar/sidebar.component';
+import { ContentComponent } from '../arquitecture/content/content.component';
+import { RinfoComponent } from '../arquitecture/rinfo/rinfo.component';
+import { PlayerComponent } from '../arquitecture/player/player.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, ContentComponent, RinfoComponent, PlayerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
