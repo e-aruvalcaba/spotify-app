c83b1bdc06b87d7d38740faf1ce76d1332a54b0a
e-aruvalcaba <adrian.ruvalcaba.sistemas@hotmail.com>
2024-05-07 02:48:22 -0600

    diseño de sidebar inicial

---
+++ spotifyapp/src/app/components/arquitecture/rinfo/rinfo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rinfo',
+  standalone: true,
+  imports: [],
+  templateUrl: './rinfo.component.html',
+  styleUrl: './rinfo.component.css'
+})
+export class RinfoComponent {
+
+}
