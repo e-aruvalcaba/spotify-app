6a306e66b508efeec564a3cc1f1e0d720df5ae23
e-aruvalcaba <adrian.ruvalcaba.sistemas@hotmail.com>
2024-05-16 04:52:20 -0600

    start component completed

--- spotifyapp/src/app/modules/left_sidebar_module/sidebar.module.ts
+++ spotifyapp/src/app/modules/left_sidebar_module/sidebar.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
-
-
+import { StartComponent } from './components/start/start.component';
+import { LibraryComponent } from './components/library/library.component';
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    StartComponent,
+    SidebarComponent,
+    LibraryComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    SidebarComponent,
+    LibraryComponent,
     SidebarComponent
   ]
 })
